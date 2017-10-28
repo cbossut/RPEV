b00cd6fd2f41085fe1fe666bf3405e324dceccac
Unknown <bossut.clement@gmail.com>
2017-10-28 16:48:56 +0200

    Interface WIP
    Lemur and node files

---
+++ Lemur/index.js
@@ -0,0 +1,73 @@
+var fs = require("fs"),
+    http = require("http"),
+    osc = require("osc"),
+    udpPort = new osc.UDPPort({
+      localAddress: "0.0.0.0",
+      localPort: 8000
+    }),
+    ipadIP = "192.168.1.41",
+    jouets = JSON.parse(fs.readFileSync("../Jouets/jouets.json")),
+    PWMWait = false
+
+//console.log(jouets)
+
+udpPort.on("ready", function() {
+  console.log("UDP Port Ready !")
+})
+
+udpPort.on("message", function(mess) {
+  //console.log("Message received :", mess)
+  
+  var addr = mess.address.split('/')
+  if (addr[1].startsWith('Jouet')) {
+    var jouet = jouets[addr[1][addr[1].length-1]]
+    if (addr[2] == "PWM" && !PWMWait) {
+      PWMWait = true
+      http.get("http://"+jouet.ip+"/PWM?v="+mess.args[0], (res)=>{
+        PWMWait = false
+        res.setEncoding('utf8')
+        res.on('data', (data)=>{
+          udpPort.send({address:"/"+addr[1]+"/Mess",args:["@content",data]},ipadIP,8000)
+        })
+      })
+    } else if (addr[2].startsWith("Btn")) {
+      console.log("Saying ", jouet[addr[2]], " to ", jouet.name)
+      http.get("http://"+jouet.ip+"/"+jouet[addr[2]], (res)=>{
+        res.setEncoding('utf8')
+        res.on('data', (data)=>{
+          console.log(data)
+          var ans = {
+            address: "/"+addr[1]+"/Mess",
+            args: ["@content", data]
+          }
+          console.log("sending ", ans)
+          udpPort.send(ans, ipadIP, 8000)
+        })
+      })
+    }
+  }
+  
+  
+  
+  
+  
+  if (mess.address == "/Switches/x") {
+    var ans = {
+          address: "/Text",
+          args: [
+            "@content",
+            mess.args[0]?"Clicked":"Noped"
+          ]
+        }
+    console.log("sending ", ans)
+    udpPort.send(ans, ipadIP, 8000)
+    http.get("http://192.168.1.10/led?v="+mess.args[0], (res)=>{res.setEncoding('utf8'),res.on('data', console.log)})
+  }
+})
+
+udpPort.open()
+
+
+
+
+
