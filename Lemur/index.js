e9de72445f85a0bf5d46ea728fda90ca233b6e97
Unknown <bossut.clement@gmail.com>
2017-10-30 00:42:09 +0100

    minor
    Debug
    Comments
    Spacing

--- Lemur/index.js
+++ Lemur/index.js
@@ -1,6 +1,6 @@
 var lemurIP = "192.168.0.31",
     lemurPort = 8000,
-    PWMMaxWait = 2,
+    PWMMaxWait = 2, //FUTURE could check max connections total for each jouet, and keep some free for btns/emergency ?
     fs = require("fs"),
     http = require("http"),
     osc = require("osc"),
@@ -14,13 +14,13 @@ for (var i = 1 ; i < jouets.length ; i++) {
   jouets[i].PWMSent = 0
 }
 
-//console.log(jouets)
-
 function sendLemur(addr, args) {
+  //console.log("Send ", args, " to Lemur's ", addr) //NOTE Debug
   udpPort.send({address:addr,args:args},lemurIP,lemurPort)
 }
 
 function sendJouet(n, path, clbk) { //TODO could be even more generic cause similar clbks
+  //console.log("Send ", path, " to ", jouets[n].name) //NOTE Debug
   http.get("http://"+jouets[n].ip+"/"+path, clbk).on('error', (err)=>{
     jouets[n].PWMSent = 0
     errClbk(err)
@@ -48,12 +48,15 @@ udpPort.on("ready", function() {
 })
 
 udpPort.on("message", function(mess) {
-  //console.log("Message received :", mess)
+  //console.log("Message received :", mess) //NOTE Debug
   
   var addr = mess.address.split('/')
   if (addr[1].startsWith('Jouet')) {
     var n = addr[1][addr[1].length-1], //TODO Warning ! n seems to be a number but is a char
         jouet = jouets[n]
+    
+    
+    
     if (addr[2] == "PWM" && addr[3] == 'x' && jouet.PWMSent < PWMMaxWait) {
       jouet.PWMSent++
       var PWMval = Math.round(mess.args[0]*(jouet.PWMBorns[1]-jouet.PWMBorns[0])+jouet.PWMBorns[0])
@@ -64,14 +67,19 @@ udpPort.on("message", function(mess) {
          sendLemur("/"+addr[1]+"/PWMValue", ["@content",data.slice(1)])
         })
       })
-      console.log("Saying ", jouet[addr[2]], " to ", jouet.name)
-    } else if (addr[2].startsWith("Btn") && (jouet[addr[2]].startsWith("toggle") || mess.args[0])) {
+      
+      
+      
+    } else if (addr[2].startsWith("Btn") && (jouet[addr[2]].startsWith("toggle") || mess.args[0])) { //TODO Strange way to manage trig and tog
       sendJouet(n, jouet[addr[2]], (res)=>{
         res.setEncoding('utf8')
         res.on('data', (data)=>{
           sendLemur("/"+addr[1]+"/Mess", ["@content", data])
         })
       })
+      
+      
+      
     } else if (addr[2] == "Reset" && mess.args[0]) {
       sendJouet(n, "reset", (res)=>{
         res.setEncoding('utf8')
