d247515724a6581f0234f1543a55c7dbdf247bf1
Gaël Jaton <33033910+gaeljaton@users.noreply.github.com>
2017-11-02 00:39:25 +0100

    Add files via upload

---
+++ UR/_6pots_Arduino/_6pots_Arduino.ino
@@ -0,0 +1,24 @@
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(115200);
+}
+
+void loop() {
+ 
+  Serial.print(analogRead(A0));
+  Serial.print(";");
+Serial.print(analogRead(A1));
+  Serial.print(";");
+  Serial.print(analogRead(A2));
+  Serial.print(";");
+  Serial.print(analogRead(A3));
+  Serial.print(";");
+  Serial.print(analogRead(A4));
+  Serial.print(";");
+  Serial.print(analogRead(A5));
+  Serial.print(";");
+ Serial.print('\r');
+ Serial.print('\n');
+  delay(50);
+}
