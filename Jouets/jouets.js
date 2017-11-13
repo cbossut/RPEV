6955ad5f2ff20d3aece7ca79b0aac9b9344f3843
Unknown <bossut.clement@gmail.com>
2017-11-13 21:52:32 +0100

    Fix previous ...

--- Jouets/jouets.js
+++ Jouets/jouets.js
@@ -16,7 +16,7 @@ for (let i = 1 ; i < jouets.length ; i++) {
 }
 
 function sendJouet(n, path, clbk) { //TODO could be even more generic cause similar clbks
-  //console.log("Send ", path, " to ", jouets[n].name) //NOTE Debug
+  //console.log("Send ", path, " to ", jouets[n].name) //DEBUG
   http.get("http://"+jouets[n].ip+"/"+path, clbk).on('error', (err)=>{
     jouets[n].PWMSent = 0
     errClbk(err)
@@ -38,9 +38,11 @@ function lemurConfig(sendLemur) {
         sendLemur("/Jouet"+i+"/"+btn, ["@behavior", 1, "@outline", 1])
       } else {//if (jouet[btn].startsWith("toggle")) {
         sendLemur("/Jouet"+i+"/"+btn, ["@behavior", 0, "@outline", 1])
+        sendLemur("/Jouet"+i+"/"+btn+"/x", 0)
       }
     }
     sendLemur("/Jouet"+i+"/Btn5", jouet.PWMInit ? ["@behavior", 1, "@outline", 1] : ["@outline", 0])
+    if (jouet.PWMInit) sendLemur("/Jouet"+i+"/PWM/x", Math.abs((jouet.PWMInit-jouet.PWMBorns[0])/(jouet.PWMBorns[1]-jouet.PWMBorns[0])))
   }
 }
 
@@ -64,13 +66,14 @@ function manageLemurMessage(mess, sendLemur) {
       
       
     } else if (addr[2].startsWith("Btn")) {
-      if (addr[2].slice('Btn'.length) == 5) {
+      if (addr[2].slice('Btn'.length) == 5 && mess.args[0]) {
         sendJouet(n, "PWM?v="+jouet.PWMInit, res => {
           res.setEncoding('utf8')
           res.on('data', data => sendLemur("/"+addr[1]+"/PWMValue", ["@content",data.slice(1)]))
         })
+        sendLemur("/"+addr[1]+"/PWM/x", Math.abs((jouet.PWMInit-jouet.PWMBorns[0])/(jouet.PWMBorns[1]-jouet.PWMBorns[0])))
       } else if (jouet[addr[2]]) {
-        if (jouet[addr[2]].startsWith("toggle") || mess.args[0]) {//TODO Strange way to manage trig and tog
+        if (jouet[addr[2]].startsWith("toggle") || (jouet[addr[2]].startsWith("trigger") && mess.args[0])) {//TODO Strange way to manage trig and tog
           sendJouet(n, jouet[addr[2]], res => {
             res.setEncoding('utf8')
             res.on('data', data => sendLemur("/"+addr[1]+"/Mess", ["@content", data]))
@@ -79,18 +82,20 @@ function manageLemurMessage(mess, sendLemur) {
           if (!jouet.Hpin) jouet.Hpin = 12
           sendJouet(n, "toggle?pin="+jouet.Hpin, res => {
             res.setEncoding('utf8')
-            res.on('data', data => {jouet.move = data.endsWith('1'); sendLemur("/"+addr[1]+"/Mess", ["@content", data])})
+            res.on('data', data => {/*jouet.move = data.endsWith('1');*/ sendLemur("/"+addr[1]+"/Mess", ["@content", data])})
           })
+          jouet.move = !jouet.move
         } else if (jouet[addr[2]] == "switch") {
           if (jouet.Hpin == 12) jouet.Hpin = 14
           else jouet.Hpin = 12
           if (jouet.move) {
-            sendJouet(n, "toggle?pin="+(jouet.Hpin==12?14:12), res => {
+            sendJouet(n, "toggle?pin="+(jouet.Hpin==12?14:12), jouet.tmp = res => {
               sendJouet(n, "toggle?pin="+jouet.Hpin, res => {
                 res.setEncoding('utf8')
                 res.on('data', data => {sendLemur("/"+addr[1]+"/Mess", ["@content", data])})
               })
             })
+            jouet.tmp({setEncoding:()=>{},on:()=>{}})
           }
         }
       }
